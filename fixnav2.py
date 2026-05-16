2ffdb2fe9a477b52a08bf941c5cd74cd18001bd5
sergiokendrick12 <sergiokendrick12@gmail.com>
2026-05-16 12:55:47 +0200

    fix: add Resources to correct position in admin sidebar

---
+++ fixnav2.py
@@ -0,0 +1,12 @@
+﻿f = open('app/admin/page.js', 'r', encoding='utf-8')
+c = f.read()
+f.close()
+
+old = '{id:"staff",        ico:"'
+new = '{id:"resources",    ico:"📚", label:"Resources",      section:"academy"},\n    {id:"staff",        ico:"'
+c = c.replace(old, new, 1)
+
+f = open('app/admin/page.js', 'w', encoding='utf-8')
+f.write(c)
+f.close()
+print('Done! resources count:', c.count('id:"resources"'))
---
+++ getnav.py
@@ -0,0 +1,8 @@
+﻿f = open('app/admin/page.js', 'r', encoding='utf-8')
+c = f.read()
+f.close()
+idx = c.find('id:"home"')
+out = open('sidebar.txt', 'w', encoding='utf-8')
+out.write(c[idx:idx+800])
+out.close()
+print('done at:', idx)
